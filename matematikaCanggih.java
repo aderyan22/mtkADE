82ef45f5a50ad7a4fe9eae2c225ba7bc8f993a71
aderyan22 <adepermadhie29@gmail.com>
2024-11-21 11:07:54 +0700

    Add files via upload

---
+++ matematikaCanggih.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package matematikaade;
+
+/**
+ *
+ * @author iv4nm
+ */
+public class matematikaCanggih extends MatematikaAde{
+    public int modulus(int a, int b) {
+        return a % b;
+    }
+}
